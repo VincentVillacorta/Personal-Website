8f3b8c31ca52a80840ac1fa0a8992d8aa708e820
VincentVillacorta <44991867+VincentVillacorta@users.noreply.github.com>
2020-03-28 01:50:35 -0700

    Recommiting master from surface

--- animations.js
+++ animations.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function(){
 
 /*! Fades in page on load */
@@ -5,3 +6,12 @@ $('body').css('display', 'none');
 $('body').fadeIn(500);
 
 });
+=======
+$(document).ready(function(){
+
+/*! Fades in page on load */
+$('body').css('display', 'none');
+$('body').fadeIn(500);
+
+});
+>>>>>>> bf1a3be182d11eaa4c058abc6bb82030da38970d
