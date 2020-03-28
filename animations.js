1f395ca260a07e6c87782e2e1a0ffa7a6421f3f2
Vince <vincentjvillacorta@gmail.com>
2020-03-28 02:06:14 -0700

    Cleaned up commits

--- animations.js
+++ animations.js
@@ -6,12 +6,3 @@ $('body').css('display', 'none');
 $('body').fadeIn(500);
 
 });
-=======
-$(document).ready(function(){
-
-/*! Fades in page on load */
-$('body').css('display', 'none');
-$('body').fadeIn(500);
-
-});
->>>>>>> bf1a3be182d11eaa4c058abc6bb82030da38970d
