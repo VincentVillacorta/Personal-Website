99c85f8f7e9f8c48e523dbed5f019faaf2ce36db
VincentVillacorta <44991867+VincentVillacorta@users.noreply.github.com>
2020-06-28 20:56:08 -0700

    OVERHAUL ON WHOLE WEBSITE

--- animations.js
+++ animations.js
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
-$(document).ready(function(){
 
-/*! Fades in page on load */
-$('body').css('display', 'none');
-$('body').fadeIn(500);
+$(document).ready(function(){
+  $("#intro-section").delay(2200).slideUp(2000,"swing");
 
 });
+
+new TypeIt(".animated-title", {
+  strings: "Software Developer"
+}).go();
