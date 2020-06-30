aff8019df6f4c5ea11495e2825a7b6a20604722a
VincentVillacorta <44991867+VincentVillacorta@users.noreply.github.com>
2020-06-29 18:01:48 -0700

    Addes new gif, toolbar, and text for main jumbotron

--- animations.js
+++ animations.js
@@ -1,9 +1,76 @@
-
 $(document).ready(function(){
   $("#intro-section").delay(2200).slideUp(2000,"swing");
-
+  $('#target').teletype({
+    text: [
+      'Software Developer',
+    ]
+  });
+  $('#cursor').teletype({
+    text: ['_', ' '],
+    delay: 0,
+    pause: 500
+  });
 });
+(function ($) {
 
-new TypeIt(".animated-title", {
-  strings: "Software Developer"
-}).go();
+  function typeString($target, str, cursor, delay, cb) {
+    $target.html(function (_, html) {
+      return html + str[cursor];
+    });
+
+    if (cursor < str.length - 1) {
+      setTimeout(function () {
+        typeString($target, str, cursor + 1, delay, cb);
+      }, delay);
+    }
+    else {
+      cb();
+    }
+  }
+  function deleteString($target, delay, cb) {
+    var length;
+
+    $target.html(function (_, html) {
+      length = html.length;
+      return html.substr(0, length - 1);
+    });
+
+    if (length > 1) {
+      setTimeout(function () {
+        deleteString($target, delay, cb);
+      }, delay);
+    }
+    else {
+      cb();
+    }
+  }
+  $.fn.extend({
+    teletype: function (opts) {
+      var settings = $.extend({}, $.teletype.defaults, opts);
+
+      return $(this).each(function () {
+        (function loop($tar, idx) {
+          // type
+          typeString($tar, settings.text[idx], 0, settings.delay, function () {
+            // delete
+            setTimeout(function () {
+              deleteString($tar, settings.delay, function () {
+                loop($tar, (idx + 1) % settings.text.length);
+              });
+            }, settings.pause);
+          });
+
+        }($(this), 0));
+      });
+    }
+  });
+  $.extend({
+    teletype: {
+      defaults: {
+        delay: 35,
+        pause: 1950,
+        text: []
+      }
+    }
+  });
+}(jQuery));
