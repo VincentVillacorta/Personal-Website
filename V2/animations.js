b69d6899964fea01504faccfa0eeb26a45249609
VincentVillacorta <44991867+VincentVillacorta@users.noreply.github.com>
2020-07-03 23:38:51 -0700

    ***PRESENTABLE Added Functionality to Buttons Issues: Jumping with menu buttons

---
+++ V2/animations.js
@@ -0,0 +1,22 @@
+var counter = 0;
+
+function aboutMe(){
+  if(counter == 0){
+    $(".text-box p1").fadeOut(function() {
+        $(".text-box p1").html("I am searching for my first software development role.").fadeIn();
+    });
+    counter = 1;
+  }
+  else if(counter == 1){
+    $(".text-box p1").fadeOut(function() {
+        $(".text-box p1").html("Contact me through the links below!").fadeIn();
+    });
+    counter = 2;
+  }
+  else{
+    $(".text-box p1").fadeOut(function() {
+        $(".text-box p1").html("I'm an aspiring software developer named Vince.").fadeIn();
+    });
+    counter = 0;
+  }
+};
